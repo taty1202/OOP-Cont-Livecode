b21cdc012080ea8cf071f0b81583cab3594ac439
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-10-01 11:06:48 -0700

    created plant class and tests

--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,1 +1,23 @@
 from classes.plant import Plant
+
+def test_plant_initialization():
+
+    # Arrange
+    name = "Butterfly pea flower"
+    water_level = 6
+    sunlight_hours = 7
+    is_blooming = True
+
+    #Act
+    plant = Plant("Butterfly pea flower", 6, 7, is_blooming)
+
+    plant_2 = Plant(name, water_level, sunlight_hours)
+
+    # Assert
+    assert plant.name == name
+    assert plant.water_level == water_level
+    assert plant.sunlight_hours == sunlight_hours
+    assert plant.is_blooming
+
+    assert not plant_2.is_blooming
+
